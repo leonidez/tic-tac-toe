df3923a01070dd32c4599cbf35cc360089d27ce5
Leonidez Acosta <acosta.leonidez@gmail.com>
2021-07-31 12:02:16 -0400

    set up left most edge

--- components/square.js
+++ components/square.js
@@ -46,6 +46,7 @@ export default function Square({ id }) {
       style={style}>
       <span 
         className={styles.letter}>
+          {id}
         {letter.toUpperCase()}
       </span>
     </animated.div>
--- modules/get-adjacent-ids.js
+++ modules/get-adjacent-ids.js
@@ -1,13 +1,41 @@
 export default function getAdjacentIds(ids) {
-  return ids.map((id) => ([
-    (id % 10 !== 0) && id - 11,
-    id - 10,
-    (id % 10 !== 9) && id - 9,
-    (id % 10 !== 0) && id - 1,
-    id,
-    (id % 10 !== 9) && id + 1,
-    (id % 10 !== 0) && id + 9,
-    id + 10,
-    (id % 10 !== 9) && id + 11,
-  ].filter((id) => id !== false)))
+  return ids.map((id) => {
+    const onLeftEdge = id % 9 === 0
+    const onRightEdge = id % 9 === 8 
+
+    return [
+      // inner ring
+
+      !onLeftEdge && id - 10,
+      id - 9,
+      id - 8,
+      !onLeftEdge && id - 1,
+      id,
+      id + 1,
+      !onLeftEdge && id + 8,
+      id + 9,
+      id + 10,
+
+      // outer ring
+
+      !onLeftEdge && id - 20,
+      !onLeftEdge && id - 19,
+      id - 18,
+      id - 17,
+      id - 16,
+
+      !onLeftEdge && id - 11,
+      id - 7,
+      !onLeftEdge && id - 2,
+      id + 2,
+      !onLeftEdge && id + 7,
+      id + 11, 
+
+      !onLeftEdge && id + 16, 
+      !onLeftEdge && id + 17, 
+      id + 18, 
+      id + 19, 
+      id + 20, 
+    ].filter((id) => id !== false)
+  })
 }
