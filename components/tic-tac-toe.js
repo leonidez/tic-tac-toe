ea6330748441147e37894068e9e695320bfccf72
Leonidez Acosta <leonidez.acosta@nytimes.com>
2021-08-04 15:49:43 -0400

    create VictoryMessage component

--- components/tic-tac-toe.js
+++ components/tic-tac-toe.js
@@ -6,6 +6,20 @@ import Square from './square'
 import getActiveSquares from '../modules/get-active-squares'
 import styles from '../styles/sheet.module.css'
 
+function VictoryMessage() {
+  const { reset, winner } = useGame()
+
+  return (
+    <>
+      <p>{winner.letter.toUpperCase()} is the winner</p>
+      <button
+        onClick={reset}>
+        Play Again
+      </button>
+    </>
+  )
+}
+
 export default function TicTacToe({ squares }) {
   const { turns, setTurns, winner } = useGame()
 
@@ -20,17 +34,20 @@ export default function TicTacToe({ squares }) {
   }, [turns.next])
 
   return (
-    <div 
-      className={styles.container}>
-      {squares.map((square) => {
-        const isWinningSquare = winner && winner.includes(square)
-        return (
-          <Square 
-            id={square}
-            key={square} 
-            isWinningSquare={isWinningSquare} />
-        )
-      })}
-    </div>
+    <>
+      {winner && <VictoryMessage />}
+      <div 
+        className={styles.container}>
+        {squares.map((square) => {
+          const isWinningSquare = winner && winner.squares.includes(square)
+          return (
+            <Square 
+              id={square}
+              key={square} 
+              isWinningSquare={isWinningSquare} />
+          )
+        })}
+      </div>
+    </>
   )
 }
