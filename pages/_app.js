3b3c2d49e7bc541993859f3f64509ad765c41daf
Leonidez Acosta <leonidez.acosta@nytimes.com>
2021-08-05 16:15:54 -0400

    preload fonts

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,8 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='preload' href='/fonts/NYTCheltenhamExtraBold.otf' as='font' crossOrigin='' />
+        <link rel='preload' href='/fonts/NYTFranklinBold.otf' as='font' crossOrigin='' />
       </Head>
       <Component {...pageProps} />
     </>
